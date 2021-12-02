6dbc7d812db39c5ea0d0ea68a0765624239e57f6
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2021-12-02 20:44:57 +0400

    Update easyloader_web.lua

--- lua/autorun/easyloader_web.lua
+++ lua/autorun/easyloader_web.lua
@@ -1,7 +1,19 @@
-local version = 0
+local CompileString = CompileString
+local string_format = string.format
+local timer_Simple = timer.Simple
+local isfunction = isfunction
+local isnumber = isnumber
+local CurTime = CurTime
+local HTTP = HTTP
+local MsgC = MsgC
+
+local version = EasyLoader and EasyLoader["_VERSION"] or 0
+local lastCheck, checkDelay = 0, 900
+
 function EasyLoaderWeb(after_load)
-    if (version == nil) or (version == 0) then
-        timer.Simple(0, function()
+    local time = CurTime()
+    if ((version == nil) or (version == 0)) or ((time - lastCheck) > checkDelay) then
+        timer_Simple(0, function()
             if (HTTP({
                 ["url"] = "https://raw.githubusercontent.com/Pika-Software/gmod_easyloader/main/lua/easyloader/core.lua",
                 ["method"] = "GET",
@@ -12,7 +24,7 @@ function EasyLoaderWeb(after_load)
                             local object = run()
                             version = EasyLoader["_VERSION"]
 
-                            print("[EasyLoader Web] " .. ((isnumber(version) and version > 0) and "OK" or "ERROR"))
+                            MsgC(Color(82, 200, 255), "[EasyLoader Web] ", Color(255, 191, 73), string_format("%s (%s)", ((isnumber(version) and version > 0) and "OK" or "ERROR"), version), "\n")
 
                             if isfunction(after_load) then
                                 after_load(object)
@@ -21,10 +33,14 @@ function EasyLoaderWeb(after_load)
                     end
                 end,
             }) == nil) then
-                print("[EasyLoader Web] Failed")
+                MsgC("[EasyLoader Web] Failed\n")
             end
         end)
+
+        lastCheck = time
     elseif isfunction(after_load) then
         after_load(object)
     end
 end
+
+EasyLoaderWeb()
